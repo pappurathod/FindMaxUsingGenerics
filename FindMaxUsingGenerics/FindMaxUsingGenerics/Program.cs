9f3ee7ec270660c044f03fa5eab068b60b26f5a6
pappurathod <prathod4545@gmail.com>
2022-03-25 16:56:16 +0530

    Find max string among three string

--- FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
@@ -9,8 +9,13 @@ namespace FindMaxUsingGenerics
             MaxIntNumber maxintnum = new MaxIntNumber();
             Console.WriteLine(maxintnum.NumberMax(10,80,10));
             Console.WriteLine("-------------------------");
+
             MaxFloatNumber maxfloatnum = new MaxFloatNumber();
             Console.WriteLine(maxfloatnum.FloatNumber(90.4f, 50.6f, 70.7f));
+            Console.WriteLine("-------------------------");
+
+            MaxString maxString= new MaxString();
+            Console.WriteLine(maxString.largeString("Apple","Peach","Banana"));
         }
     }
 }
