669f5a06b793e72aa0d75925f5db55e0bd3f02b4
pappurathod <prathod4545@gmail.com>
2022-03-25 16:12:23 +0530

    Find maximum number among three integer number

---
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/MaxIntNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaxUsingGenerics
+{
+    internal class MaxIntNumber
+    {
+        public int NumberMax(int num1, int num2, int num3)
+        {
+            Console.WriteLine("Three numbers are num1 = {0} , num2 = {1} , num3 = {2} ", num1, num2, num3);
+
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                Console.WriteLine("Max = ");
+                return num1;
+            }
+            else if (num2.CompareTo(num3) > 0 && num2.CompareTo(num1) > 0)
+            {
+                Console.WriteLine("Max = ");
+                return num2;
+            }
+            else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            {
+                Console.WriteLine("Max = ");
+                return num3;
+            }
+            else
+            {
+                Console.WriteLine("all numbers are equal");
+            }return 0;
+        }
+    }
+}
---
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FindMaxUsingGenerics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+             MaxIntNumber maxintnum = new MaxIntNumber();
+             Console.WriteLine(maxintnum.NumberMax(10,80,10));
+        }
+    }
+}
