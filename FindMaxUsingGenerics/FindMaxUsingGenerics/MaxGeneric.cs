02ff292303a6fcf982c5a178a00c77f0800df9ec
pappurathod <prathod4545@gmail.com>
2022-03-27 09:41:59 +0530

    Refactor code using generics and constructor

--- FindMaxUsingGenerics/FindMaxUsingGenerics/MaxGeneric.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/MaxGeneric.cs
@@ -8,30 +8,37 @@ namespace FindMaxUsingGenerics
 {
     public class MaxGeneric<T> where T : IComparable
     {
-        public T maxLength(T value_1, T value_2, T value_3)
+        public T value_1, value_2, value_3;
+        public MaxGeneric(T value_1, T value_2, T value_3)
+        {
+            this.value_1 = value_1;
+            this.value_2 = value_2;
+            this.value_3 = value_3;
+        }
+
+        public void testMaximum()
         {
             T max = value_1;
             if (value_1.CompareTo(value_2) > 0 && value_1.CompareTo(value_3) > 0)
             {
                 max = value_1;
-                Console.Write("Max length = {0}\n", max);
+                Console.Write("Maximum is = {0}\n", max);
             }
             else if (value_2.CompareTo(value_1) > 0 && value_2.CompareTo(value_3) > 0)
             {
                 max = value_2;
-                Console.Write("Max length = {0}\n", max);
+                Console.Write("Maximum is = {0}\n", max);
             }
             else if (value_3.CompareTo(value_1) > 0 && value_3.CompareTo(value_2) > 0)
             {
 
                 max = value_3;
-                Console.Write("Max length = {0}\n", max);
+                Console.Write("Maximum is = {0}\n", max);
             }
             else
             {
                 Console.WriteLine("all are same");
-            }
-            return max;
+            }            
         }
     }
 }
--- FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
+++ FindMaxUsingGenerics/FindMaxUsingGenerics/Program.cs
@@ -2,18 +2,18 @@
 
 namespace FindMaxUsingGenerics
 {
-    class Program
-    {
+    public class Program
+    {    
+        
         public static void Main(string[] args)
         {
-            MaxGeneric<int> maxGenericInt = new MaxGeneric<int>();
-            maxGenericInt.maxLength(10, 10, 10);
-
-            MaxGeneric<float> maxGenericFloat = new MaxGeneric<float>();
-            maxGenericFloat.maxLength(10.55f, 55.55f, 33.54f);
-
-            MaxGeneric<string> maxGenericStr = new MaxGeneric<string>();
-            maxGenericStr.maxLength("Apple", "Peach", "Banana");
+            MaxGeneric<int> intValue = new MaxGeneric<int>(77, 44, 99);
+            intValue.testMaximum();
+            MaxGeneric<float> floatValue = new MaxGeneric<float>(15.33f, 33.44f, 15.55f);
+            floatValue.testMaximum();
+            MaxGeneric<string> strValue = new MaxGeneric<string>("Apple", "Peach", "Banana");
+            strValue.testMaximum(); 
         }
+
     }
 }
